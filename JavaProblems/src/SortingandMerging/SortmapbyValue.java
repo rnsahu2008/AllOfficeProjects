ef47db619a6438f0c1c3ddcb482f83cfa09d2b14
Ram.Sahu <ram.sahu0823@gmail.com>
2017-05-07 17:55:51 +0530

    commit

--- JavaProblems/src/SortingandMerging/SortmapbyValue.java
+++ JavaProblems/src/SortingandMerging/SortmapbyValue.java
@@ -1,36 +1,55 @@
 package SortingandMerging;
 
 	import java.util.ArrayList;
-	import java.util.Collections;
-	import java.util.Comparator;
-	import java.util.HashMap;
-	import java.util.List;
-	import java.util.Map;
-	import java.util.Set;
-	import java.util.Map.Entry;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.codehaus.jackson.map.util.Comparators;
 	 
 	public class SortmapbyValue {
 	 
 	    public static void main(String a[]){
-	        Map<String, Integer> map = new HashMap<String, Integer>();
-	        map.put("java", 20);
-	        map.put("C++", 45);
-	        map.put("Java2Novice", 2);
-	        map.put("Unix", 67);
-	        map.put("MAC", 26);
-	        map.put("Why this kolavari", 93);
-	        Set<Entry<String, Integer>> set = map.entrySet();
-	        List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(set);
-	        Collections.sort( list, new Comparator<Map.Entry<String, Integer>>()
-	        {
-	            public int compare( Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2 )
-	            {
-	                return (o2.getValue()).compareTo( o1.getValue() );
-	            }
-	        } );
-	        for(Map.Entry<String, Integer> entry:list){
-	            System.out.println(entry.getKey()+" ==== "+entry.getValue());
-	        }
+	        Map<String, String> map = new HashMap<String, String>();
+	        map.put("w", "y");
+	        map.put("z", "z");
+	        map.put("b", "a");
+	        map.put("a", "x");
+	        map.put("b", "f");
+	        map.put("n", "p");
+	  
+	        Map<String, Integer> map1 = new HashMap<String, Integer>();
+	        map1.put("w", 11);
+	        map1.put("z", 21);
+	        map1.put("b", 1);
+	        map1.put("a", 1);
+	        map1.put("m", 7);
+	        map1.put("n", 1);
+	  
+List<Entry<String,Integer>> list2 = new ArrayList<Entry<String,Integer>>(map1.entrySet());
+Collections.sort(list2,new Comparator<Entry<String,Integer>>() 
+		{
+	public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2)
+	{
+		
+		return o1.getValue().compareTo(o2.getValue());
+	}
+});
+
+	    
+System.out.println(Arrays.asList(list2));
+
+for(Entry<String,Integer> entr:list2)
+{
+	if(entr.getValue()==1)
+	System.out.println(entr.getKey()+"====="+entr.getValue());
+
+}
+
 	    }
 	}
-
